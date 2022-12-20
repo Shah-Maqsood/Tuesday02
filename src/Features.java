2db466c822218f34c7d637981058f8e6fb326f50
Shah-Maqsood <koelnbonn99@gmail.com>
2022-12-20 13:45:18 +0100

    My new comments on the project

---
+++ src/Features.java
@@ -0,0 +1,5 @@
+public class Features {
+    String weatherCondition;
+    int centiGrade;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Features xx=new Features();
+        xx.centiGrade=37;
+        xx.weatherCondition="Sunney";
+
+
+        System.out.println("Hello world!");
+    }
+}
